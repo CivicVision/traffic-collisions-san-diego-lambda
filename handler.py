6123779346bdc4c4d2463797c0bf09874d891734
milafrerichs <mila.frerichs@gmail.com>
2018-06-07 07:59:06 +0200

    Update loading of data
    change the way tweets work
    use the latest data and differtiate between collision, injury or
    fatality

--- handler.py
+++ handler.py
@@ -82,20 +82,10 @@ def traffic_collissions(event, context):
     return response
 
 def tweet_fatality(event, context):
-    data = {}
-    data['table'] = load_data.load_2017_killed_data()
-    data['last'] = load_data.load_last_fatality()
-
-    new_last = data['table'].order_by('date_time', reverse=True).limit(1)
-    old_last = data['last'].order_by('date_time', reverse=True).limit(1)
-    new_report_id = new_last_row.get('report_id')
-    old_report_id = old_last_row.get('report_id')
-    if old_report_id is not new_report_id:
-        tweet.tweet_fatality(new_last.rows.values()[0])
-        upload.upload_table(new_last, 'last_fatality.csv')
+    result = tweet.tweet_last_fatality()
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps(result)
     }
 
     return response
